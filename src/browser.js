c3b2ff83ddca56d4c927731dcd025f69bc96eaab
why520crazy <why520crazy@163.com>
2017-10-25 16:33:55 +0800

    0.0.3

--- src/browser.js
+++ src/browser.js
@@ -1,15 +1,25 @@
-var defaultLanguage = 'en-us';
+var defaultLanguage = 'zh-cn';
 var supportLanguages = [
     'zh-cn',
     'zh-tw',
     'en-us'
 ];
+var EN_REGEX = /^en/g;
 
 export default {
+    setDefaultLanguage: function(language) {
+        defaultLanguage = language;
+    },
+    setSupportLanguages: function(languages) {
+        supportLanguages = languages
+    },
     getLanguage: function() {
-        var language = (navigator.language || navigator.userLanguage).toLowerCase();
+        var language = (navigator.language || navigator.userLanguage);
+        language = language ? language.toLowerCase() : '';
         if (supportLanguages.indexOf(language) >= 0) {
             return language;
+        } else if (EN_REGEX.test(language)) {
+            return 'en-us';
         } else {
             return defaultLanguage;
         }
